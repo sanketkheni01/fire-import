9a55ef18109ccf497c0057edb9337a214d5bdace
sanketkheni01 <68942390+sanketkheni01@users.noreply.github.com>
2022-03-06 13:35:38 +0530

    .

--- src/commands/firestore.ts
+++ src/commands/firestore.ts
@@ -2,30 +2,41 @@ import { execute } from '@yarnpkg/shell'
 import chalk from 'chalk'
 import ora from 'ora'
 import prompt from 'prompt'
+import {
+  DownloadFolderFromBucket,
+  ExportFirestoreToBucket,
+  GCPLogin,
+  setGCPProjectId,
+  StartFirebaseEmulatorCommand,
+} from '../Constants/commands'
 
 async function firestore() {
-  const { projectId } = await prompt.get(['projectId'])
+  // ? Get the firebase project_id
+  const { projectId }: { projectId: string } = await prompt.get(['projectId'])
   const spinner = ora('Importing data').start()
-  await execute(`gcloud auth login`)
-  await execute(`gcloud config set project ${projectId}`)
-  await execute(
-    `gcloud firestore export gs://${projectId}.appspot.com/firebaseExport`
+
+  // * Login with GCloud
+  await execute(GCPLogin)
+
+  // * Set project id google cloud
+  await execute(setGCPProjectId(projectId))
+
+  // * Export firestore data to google cloud bucket (firebase storage)
+  await execute(ExportFirestoreToBucket(projectId))
+
+  // * Get folder from google cloud bucket to local storage
+  const exitCodeImport: number = await execute(
+    DownloadFolderFromBucket(projectId)
   )
-  const exitCodeImport = await execute(
-    `gsutil -m cp -r gs://${projectId}.appspot.com/firebaseExport .`
-  )
-  console.log(exitCodeImport)
+
+  // ? only run if the data is imported successfully
   if (exitCodeImport === 0) {
     spinner.succeed('Import successful')
     console.log(chalk.yellowBright('FireStore data imported 🔥🔥🔥🎉🎉🎉'))
     console.log(
       chalk.green('Run following command to import data to fireStore')
     )
-    console.log(
-      chalk.black.bgYellow(
-        'firebase emulators:start --import=./firebaseExport --export-on-exit=./firebaseExport'
-      )
-    )
+    console.log(chalk.black.bgYellow(StartFirebaseEmulatorCommand))
     spinner.stop()
   } else {
     spinner.fail('Import failed')
