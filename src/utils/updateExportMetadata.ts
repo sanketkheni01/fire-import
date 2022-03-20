48f7b1645697cbca534c54465c365b52a934c5ab
sanketkheni01 <68942390+sanketkheni01@users.noreply.github.com>
2022-03-20 21:01:11 +0530

    .

--- src/utils/updateExportMetadata.ts
+++ src/utils/updateExportMetadata.ts
@@ -2,24 +2,24 @@ import fs from 'fs-extra'
 import createFirebaseExportFile from './createFirebaseExportFile.js'
 
 export default async function updateExportMetadata(key: string, value: any) {
-  let isFileExists = await fs.pathExists(
-    './firebaseExport/firebase-export-metadata1.json'
-  )
-  if (isFileExists) {
-    await updateData(key, value)
-  } else {
-    await createFirebaseExportFile()
-    await updateData(key, value)
-  }
+	let isFileExists = await fs.pathExists(
+		'./firebaseExport/firebase-export-metadata.json'
+	)
+	if (isFileExists) {
+		await updateData(key, value)
+	} else {
+		await createFirebaseExportFile()
+		await updateData(key, value)
+	}
 }
 
 async function updateData(key: string, value: string) {
-  let metadataExport = await fs.readJson(
-    './firebaseExport/firebase-export-metadata.json'
-  )
-  metadataExport[key] = value
-  await fs.writeJson(
-    './firebaseExport/firebase-export-metadata.json',
-    metadataExport
-  )
+	let metadataExport = await fs.readJson(
+		'./firebaseExport/firebase-export-metadata.json'
+	)
+	metadataExport[key] = value
+	await fs.writeJson(
+		'./firebaseExport/firebase-export-metadata.json',
+		metadataExport
+	)
 }
